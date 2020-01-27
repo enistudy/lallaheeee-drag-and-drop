2415e7d51a9c316eb6e9e06a64cbab976481d77e
lallaheeee <ooop0422@gmail.com>
2020-01-27 20:49:50 +0900

    [feat] dragdrop context의 초기 값 더미데이터 작성

---
+++ client/src/components/DragDrop/initialValue.js
@@ -0,0 +1,36 @@
+const initialValue = {
+  draggedId: null,
+  draggedIndex: null,
+  draggableList: {
+    'drop-1': [
+      {
+        draggableId: '1',
+      },
+      {
+        draggableId: '2',
+      },
+      {
+        draggableId: '3',
+      },
+      {
+        draggableId: '4',
+      },
+    ],
+    'drop-2': [
+      {
+        draggableId: '5',
+      },
+      {
+        draggableId: '6',
+      },
+      {
+        draggableId: '7',
+      },
+      {
+        draggableId: '8',
+      },
+    ],
+  },
+};
+
+export default initialValue;
